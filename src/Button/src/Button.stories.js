5ba87c8a6087e6ce007834b38afba64b0ff5ff43
darshan gurubaxani <darshan.gurubaxani803@gmail.com>
2022-03-10 03:11:50 -0600

    text update

--- src/Button/src/Button.stories.js
+++ src/Button/src/Button.stories.js
@@ -3,5 +3,5 @@ import { storiesOf } from "@storybook/react";
 import Button from '@audnavlibdarshan/button';
 
 storiesOf("Button", module).add("Default", () => (
-    <Button onClick={() => { alert('You are the best friend! =)')}}>Click me</Button>
+    <Button onClick={() => { alert('You are the best friend buddy! =)')}}>Click me</Button>
 ));
