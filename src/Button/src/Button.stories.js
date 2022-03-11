39b92d8cf3fe68dc0c6c90195c2991a3117965ac
darshan gurubaxani <darshan.gurubaxani803@gmail.com>
2022-03-11 03:16:13 -0600

    test

--- src/Button/src/Button.stories.js
+++ src/Button/src/Button.stories.js
@@ -3,5 +3,5 @@ import { storiesOf } from "@storybook/react";
 import Button from '@darshuslib/button';
 
 storiesOf("Button", module).add("Default", () => (
-    <Button onClick={() => { alert('You are the best buddy sir! =)')}}>Click me</Button>
+    <Button onClick={() => { alert('You are the best buddy sirji! =)')}}>Click me</Button>
 ));
